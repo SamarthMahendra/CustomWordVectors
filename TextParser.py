9e608781ab240a79adce29fa1cafd8e066130668
M Samarth Mahendra <39624187+SamarthMahendra@users.noreply.github.com>
2024-09-17 21:40:46 -0400

    dimention reduction and plotting

--- TextParser.py
+++ TextParser.py
@@ -12,25 +12,46 @@ class TextParser:
         import re
         # lower case
         self.text = self.text.lower()
+        import nltk
+        from nltk.corpus import stopwords
+        nltk.download('stopwords')
+        stop_words = set(stopwords.words('english'))
+        self.text = ' '.join([word for word in self.text.split() if word not in stop_words])
+        self.text = self.text.split('\n')
+        self.text = ' . '.join(self.text)
         # split by . ?
         self.tokens = re.split(r'[.?!]\s*', self.text)
         # remove all non-alphanumeric characters
-        for i in range(len(self.tokens)):
-            self.tokens[i] = ''.join(e for e in self.text if e.isalnum() or e.isspace())
+        for sentence in range(len(self.tokens)):
+
+
+            self.tokens[sentence] = ''.join(e for e in self.tokens[sentence] if e.isalnum() or e.isspace()).split()
+
 
         return self.tokens
 
 
 
     def create_dictonary_tokens(self):
+        token_dict = set()
         # lower case
         self.text = self.text.lower()
+        # remove stop words
+        import nltk
+        from nltk.corpus import stopwords
+        nltk.download('stopwords')
+        stop_words = set(stopwords.words('english'))
+        self.text = ' '.join([word for word in self.text.split() if word not in stop_words])
+        # remove \n
+        self.text = self.text.replace('\n', ' ')
         # remove all non-alphanumeric characters
         self.text = ''.join(e for e in self.text if e.isalnum() or e.isspace())
         # tokenize
         self.tokens = self.text.split()
 
-        return list(set(self.tokens))
+        for token in self.tokens:
+            token_dict.add(token)
+        return list(token_dict)
 
     def parse(self):
         self.text = self.file_reader.read()
